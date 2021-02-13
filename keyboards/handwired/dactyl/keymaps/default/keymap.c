ec6cf30ed60cb89169bd7d14b8f1ba542c1b63b2
Dan Wilkerson <notdanwilkerson@gmail.com>
2021-02-13 18:58:04 -0500

    Removed other debug flags

--- keyboards/handwired/dactyl/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl/keymaps/default/keymap.c
@@ -150,18 +150,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 // Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
-
-};
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
-}
-
+void matrix_init_user(void) {};
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {};
